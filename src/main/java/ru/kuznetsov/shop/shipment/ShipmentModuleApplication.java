8d7657ebf83afd9614f4b5c70938e90b6d7a90f0
Pavel Kuznetsov <Technic47@gmail.com>
2025-12-24 15:35:32 +0300

    Init commit,

---
+++ src/main/java/ru/kuznetsov/shop/shipment/ShipmentModuleApplication.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.shipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipmentModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kuznetsov/shop/shipment/ShipmentModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.shipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
